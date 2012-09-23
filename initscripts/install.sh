f48467b2088ded95e42faab9c405b738b24ca7bc
Martin Pärtel <martin.partel@gmail.com>
2012-09-23 16:22:03 +0300

    Init script: run jetty as the user owning the tmc-comet directory.

--- initscripts/install.sh
+++ initscripts/install.sh
@@ -4,11 +4,16 @@ BASEDIR=`dirname "$THISDIR"`
 TEMPLATE="$THISDIR/template.txt"
 SCRIPT_NAME=tmc-comet
 TARGET="/etc/init.d/$SCRIPT_NAME"
+RUN_AS=`stat -c '%U' "$BASEDIR"`
 
 which nc > /dev/null || (echo "Error: 'nc' command missing! Please install netcat."; exit 1)
 
-echo "Installing into $TARGET"
-cat "$TEMPLATE" | sed "s@__BASEDIR__@$BASEDIR@" | sed "s@__NAME__@$SCRIPT_NAME@" > $TARGET
+echo "Installing into $TARGET."
+echo "Configuring it to run as $RUN_AS (the current owner of the directory)."
+cat "$TEMPLATE" |
+  sed "s@__BASEDIR__@$BASEDIR@" |
+  sed "s@__NAME__@$SCRIPT_NAME@" |
+  sed "s@__USER__@$RUN_AS@" > $TARGET
 chmod a+x "$TARGET"
 
 echo "Setting to start/stop by default"
